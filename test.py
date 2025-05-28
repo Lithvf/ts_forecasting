da466d2294f8f805ff60601c8bf37486edd2f8fc
Lithvf <lithvf@gmail.com>
2025-05-28 17:17:10 +0200

    removed script that had no value

--- test.py
+++ test.py
@@ -18,30 +18,28 @@ if __name__ == "__main__":
         split_data.test, target="PJME_MW", purpose="training", horizon=horizon
     ).get_features_and_target()
 
-    # print(split_data.test)
-    param_grid = {
-        "n_estimators": [100, 200],
-        "learning_rate": [0.05, 0.1],
-        "max_depth": [3, 5],
-    }
+    # param_grid = {
+    #     "n_estimators": [100, 200],
+    #     "learning_rate": [0.05, 0.1],
+    #     "max_depth": [3, 5],
+    # }
 
     trained_model = Model(XGBRegressor)
+    trained_model.train(X_train, y_train)
 
-    trained_model.train(X_train, y_train, split_data.train, param_grid=param_grid)
-    # reporting = MetricsReporting(
-    #     trained_model.y_pred_train, trained_model.y_train
-    # ).print()
-    # reporting.plot_metrics()
+    reporting = MetricsReporting(
+        trained_model.y_pred_train, trained_model.y_train, purpose="train"
+    ).visualize_metrics()
 
-    # PlotPredictionActual(
-    #     split_data.train, trained_model.y_pred_train, "01-03-2010"
-    # ).plot_sample()
+    PlotPredictionActual(
+        split_data.train, trained_model.y_pred_train, "01-11-2014"
+    ).plot_sample()
 
     forecaster = MultiHorizonForecaster(
         trained_model.trained_model, split_data.train, horizon=horizon
     )
     forecast = forecaster.forecaster()
-    reporting = MetricsReporting(forecast, split_data.test)
-    reporting.print()
-    # reporting.plot_metrics()
+    reporting = MetricsReporting(
+        forecast, split_data.test, purpose="test"
+    ).visualize_metrics()
     PlotPredictionActual(split_data.test, forecast).plot_sample()

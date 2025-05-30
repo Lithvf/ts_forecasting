692b3a98f847856df9d1414f62a698763c1c4c21
Lithvf <lithvf@gmail.com>
2025-05-30 16:49:30 +0200

    updated train_model for evaluation with mlflow, needs to be modularized

--- train_model.py
+++ train_model.py
@@ -13,18 +13,18 @@ def load_config(path="config.yaml"):
         return yaml.safe_load(f)
 
 
-if __name__ == "__main__":
+def main_train(config, purpose):
     config = load_config("config.yaml")
-    mlflow.set_experiment("EnergyForecasting_MultiHorizon")
+    mlflow.set_experiment("Multi horizon - Energy forecast")
 
-    with mlflow.start_run():
+    with mlflow.start_run() as run:
+        run_id = run.info.run_id
         model_name = config["model"]["name"]
         grid_params = config["model"]["grid_params"]
         lag_features = config["features"]["lag_features"]
         timestamp_split = config["split"]["timestamp_start"]
         horizon = config["forecasting"]["horizon"]
 
-        # Log parameters
         mlflow.log_param("model_name", model_name)
         mlflow.log_param("horizon", horizon)
         mlflow.log_params(grid_params)
@@ -49,15 +49,75 @@ if __name__ == "__main__":
 
         print(metrics)
 
-        # Log overall metrics
         mlflow.log_metric("rmse_overall", float(metrics["rmse_overall"]))
         mlflow.log_metric("r2_overall", metrics["r2_overall"])
 
         # Log per-horizon metrics
         for i, horizon in enumerate(metrics["horizon"]):
-            mlflow.log_metric(f"rmse_{horizon}", float(metrics["rmse_per_horizon"][i]))
-            mlflow.log_metric(f"r2_{horizon}", metrics["r2_per_horizon"][i])
-            mlflow.log_metric(f"mape_{horizon}", float(metrics["mape_per_horizon"][i]))
+            mlflow.log_metric(
+                f"rmse_{purpose}_{horizon}", float(metrics["rmse_per_horizon"][i])
+            )
+            mlflow.log_metric(f"r2_{purpose}_{horizon}", metrics["r2_per_horizon"][i])
+            mlflow.log_metric(
+                f"mape_{purpose}_{horizon}", float(metrics["mape_per_horizon"][i])
+            )
+
+    return run_id
+
+
+def main_test(run_id, purpose):
+    config = load_config("config.yaml")
+    mlflow.set_experiment("Multi horizon - Energy forecast")
+
+    with mlflow.start_run(run_id=run_id) as run:
+        model_name = config["model"]["name"]
+        grid_params = config["model"]["grid_params"]
+        lag_features = config["features"]["lag_features"]
+        timestamp_split = config["split"]["timestamp_start"]
+        horizon = config["forecasting"]["horizon"]
+
+        mlflow.log_param("model_name", model_name)
+        mlflow.log_param("horizon", horizon)
+        mlflow.log_params(grid_params)
+        mlflow.log_param("lag_features", str(lag_features))
+
+        raw_data = EnergyUsageData(path="data/PJME_hourly.csv").get_data()
+        split_data = SplitData(raw_data, timestamp_split, plot=False)
+        X_train, y_train = CreateFeatures(
+            split_data.train,
+            lag_features,
+            target="PJME_MW",
+            purpose="training",
+            horizon=horizon,
+        ).get_features_and_target()
+
+        trained_model = Model(XGBRegressor)
+        trained_model.train(X_train, y_train)
+
+        metrics = MetricsReporting(
+            trained_model.y_pred_train, trained_model.y_train, purpose="train"
+        ).calculate_metrics()
+
+        print(metrics)
+
+        mlflow.log_metric("rmse_overall", float(metrics["rmse_overall"]))
+        mlflow.log_metric("r2_overall", metrics["r2_overall"])
+
+        # Log per-horizon metrics
+        for i, horizon in enumerate(metrics["horizon"]):
+            mlflow.log_metric(
+                f"rmse_{purpose}_{horizon}", float(metrics["rmse_per_horizon"][i])
+            )
+            mlflow.log_metric(f"r2_{purpose}_{horizon}", metrics["r2_per_horizon"][i])
+            mlflow.log_metric(
+                f"mape_{purpose}_{horizon}", float(metrics["mape_per_horizon"][i])
+            )
+
+
+if __name__ == "__main__":
+    config = load_config("config.yaml")
+    run_id = main_train(config, purpose="train")
+    main_test(run_id, purpose="test")
 
     # reporting = MetricsReporting(
     #     trained_model.y_pred_train, trained_model.y_train, purpose="train"
